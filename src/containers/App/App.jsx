e02152dee9b31ffdf60aa77d2a310da37cd0572d
voitekhovich <a.voitekhovich@gmail.com>
2023-06-13 17:15:20 +0300

    fix: styles

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,23 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routeConfig from "@routes/routeConfig";
-import Header from '@components/Header'
+import Header from "@components/Header";
 
-// import styles from "./App.module.css";
+import styles from "./App.module.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        {routeConfig.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            end={route.end}
-            element={route.element}
-          />
-        ))}
-      </Routes>
+      <div className={styles.wraper}>
+        <Header />
+        <Routes>
+          {routeConfig.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              end={route.end}
+              element={route.element}
+            />
+          ))}
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
