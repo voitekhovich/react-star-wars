1a6e2c74b2055c3ebf0d4ea7a7bec29aef032ef2
voitekhovich <a.voitekhovich@gmail.com>
2023-06-22 22:34:58 +0300

    feat: add in personPage hook withErrorApi

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,13 +1,28 @@
 import { useEffect } from "react";
-import { useParams   } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import { withErrorApi } from "@hoc-helpers/withErrorApi";
 import { API_PERSON } from "@constants/api";
+import { getApiResource } from "@utils/network";
 import styles from "./PersonPage.module.css";
 
-const PersonPage = () => {
+const PersonPage = ({ setErrorApi }) => {
   let params = useParams();
-  console.log(params.id);
-  
+
+  useEffect(() => {
+    (async () => {
+      const res = await getApiResource(`${API_PERSON}/${params.id}`);
+      if (res) setErrorApi(false);
+      else setErrorApi(true);
+    })();
+  }, []);
+
   return <div>PersonPage</div>;
 };
 
-export default PersonPage;
+PersonPage.propTypes = {
+  setErrorApi: PropTypes.func,
+};
+
+export default withErrorApi(PersonPage);
