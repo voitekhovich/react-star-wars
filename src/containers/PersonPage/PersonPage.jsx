dde487ee74c64873d0eeae9b756283793d7cd26f
voitekhovich <a.voitekhovich@gmail.com>
2023-06-26 16:56:06 +0300

    feat: add person info

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,24 +1,66 @@
-import { useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import { withErrorApi } from "@hoc-helpers/withErrorApi";
+import { getPeopleImage } from "@services/getPeopleData";
 import { API_PERSON } from "@constants/api";
 import { getApiResource } from "@utils/network";
 import styles from "./PersonPage.module.css";
 
 const PersonPage = ({ setErrorApi }) => {
   let params = useParams();
+  const [personInfo, setPersonInfo] = useState([]);
+  const [personName, setPersonName] = useState();
+  const [personPhoto, setPersonPhoto] = useState();
 
   useEffect(() => {
     (async () => {
       const res = await getApiResource(`${API_PERSON}/${params.id}`);
-      if (res) setErrorApi(false);
-      else setErrorApi(true);
+      if (res) {
+        // console.log(res);
+
+        setPersonInfo([
+          { title: "Birth Year", data: res.birth_year },
+          { title: "Eye Color", data: res.eye_color },
+          { title: "Gender", data: res.gender },
+          { title: "Hair Color", data: res.hair_color },
+          { title: "Height", data: res.height },
+          { title: "Mass", data: res.mass },
+          { title: "Skin Color", data: res.skin_color },
+        ]);
+        setPersonName(res.name);
+        setPersonPhoto(getPeopleImage(params.id))
+
+        // res.films
+
+        setErrorApi(false);
+      } else {
+        setErrorApi(true);
+      }
     })();
   }, []);
 
-  return <div>PersonPage</div>;
+  return (
+    <Fragment>
+      <h1 className={styles.color}>{personName}</h1>
+      <img src={personPhoto} alt={personName} />
+      {personInfo && (
+        <ul className={styles.color}>
+          {personInfo.map(
+            ({ title, data }) =>
+              data && (
+                <li key={title}>
+                  <span>
+                    {title}: {data}
+                  </span>
+                </li>
+              )
+          )}
+        </ul>
+      )}
+    </Fragment>
+  );
 };
 
 PersonPage.propTypes = {
